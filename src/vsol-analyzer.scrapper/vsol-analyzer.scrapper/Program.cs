dc00bb6b525dc3c2a8968f49fbf9060cb0e2dcaa
Anatolii Gabuza (AG) <anatolii.gabuza@xe05.ey.com>
2017-03-29 15:23:09 +0200

    initail commit

---
+++ src/vsol-analyzer.scrapper/vsol-analyzer.scrapper/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using OpenScraping;
+using OpenScraping.Config;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace vsol_analyzer.scrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonConfig = File.ReadAllText(@"match-result.config.json");
+            var config = StructuredDataConfig.ParseJsonString(jsonConfig);
+
+            var html = string.Empty;
+            using (WebClient client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                html = client.DownloadString("http://virtualsoccer.ru/viewmatch.php?day=12968&match_id=213340");
+            }
+
+            var openScraping = new StructuredDataExtractor(config);
+            var scrapingResults = openScraping.Extract(html);
+
+            Console.WriteLine(JsonConvert.SerializeObject(scrapingResults, Formatting.Indented));
+            Console.ReadKey();
+        }
+    }
+}
